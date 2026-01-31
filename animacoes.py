1e8a0e97e417c1184e275a9e2e2d0844a3b207fb
Pedro Henrique <pedrohenriquenasck@gmail.com>
2026-01-31 12:26:54 -0300

    Refactor animacao2D to support multiple grid types
    Updated the animation function to handle different grids and added visual indicators for the guide in the plot.

--- animacoes.py
+++ animacoes.py
@@ -11,7 +11,7 @@ E_z, H_x, H_y = None, None, None
 
 """
 
-    • Função que gera as animações 2D do projeto
+    • Função que gera as animações 2D do projeto.
 
 """
 
@@ -19,9 +19,17 @@ def animacao2D():
     
     global E_z, H_x, H_y
     
-    # REINICIALIZA OS CAMPOS PARA COMEÇAR A ANIMAÇÃO DO ZERO
+    # VERIFICA QUAL GRID USAR E REINICIALIZA OS CAMPOS PARA COMEÇAR A ANIMAÇÃO DO ZERO
     
-    E_z, H_x, H_y = calc.inicializarCampos()
+    if cts.LETRA == 'C':
+    
+        E_z, H_x, H_y = calc.inicializarCamposAntena()
+        altura_plot = cts.ALTURA_MUNDO_C
+    
+    else:
+       
+        E_z, H_x, H_y = calc.inicializarCampos()
+        altura_plot = cts.A_LARGURA
     
     # CONFIGURAÇÃO DO TEMPO
 
@@ -36,8 +44,8 @@ def animacao2D():
 
     # CONFIGURAÇÃO DA FIGURA
 
-    figura, ax = mp.subplots(figsize=(10, 4))
-    im = ax.imshow(E_z, cmap = 'RdBu', vmin = -0.5, vmax = 0.5, origin = 'lower', extent = [0, cts.COMPRIMENTO_Z * 1000, 0, cts.A_LARGURA * 1000], animated = True)
+    figura, ax = mp.subplots(figsize=(10, 6))
+    im = ax.imshow(E_z, cmap = 'RdBu', vmin = -0.1, vmax = 0.1, origin = 'lower', extent = [0, cts.COMPRIMENTO_Z * 1000, 0, altura_plot * 1000], animated = True)
     titulo = f"Propagação TE10 - {cts.FREQUENCIA_OPERACAO / 1e9:.2f} GHz"
     
     if cts.LETRA == 'B':
@@ -50,6 +58,14 @@ def animacao2D():
     cbar.set_label("Amplitude do Campo Elétrico (Ey)")
 
     tempo_texto = ax.text(0.02, 0.90, '', transform = ax.transAxes, color = 'black', bbox = dict(facecolor = 'white', alpha = 0.7))
+
+    # DESENHA LINHAS VERDES PARA MOSTRAR ONDE ESTÁ O GUIA (AJUDA A VER AS FENDAS) 
+    
+    if cts.LETRA == 'C':
+        
+        y_guia = cts.A_LARGURA * 1000
+        ax.axhline(y = y_guia, color = 'green', linewidth = 1, linestyle = '--')
+        ax.text(0.5, y_guia + 0.2, "Parede com Fendas", color = 'green')
 
     # FUNÇÃO DE ATUALIZAÇÃO INTERNA
     
@@ -68,7 +84,13 @@ def animacao2D():
             
             # A FONTE TOMA A DECISÃO DE QUAL USAR
 
-            E_z = calc.fonte(E_z, tempo)
+            if cts.LETRA == 'C':
+
+                E_z = calc.aplicarCondicoesAntena(E_z) # DESENHA AS FENDAS
+                E_z = calc.fonteAntena(E_z, tempo)         
+            
+            else:
+                E_z = calc.fonte(E_z, tempo)
         
         im.set_array(E_z)
         tempo_atual_ps = frame * passos_frame * cts.TEMPO * 1e12
